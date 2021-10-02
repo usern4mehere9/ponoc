f11de2ed0e47058cfd2b9c3075c1e02c38041e5e
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-02 14:26:52 +0200

    Add files via upload

---
+++ ponoc/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ponoc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int h, m, s, pp, sph, spm, sps, aux; 
+
+            h = int.Parse(Console.ReadLine());
+            m = int.Parse(Console.ReadLine());
+            s = int.Parse(Console.ReadLine());
+
+            pp = (h * 3600) + (m * 60) + s;
+   
+            aux = 86400 - pp;
+            sph = aux / 3600;
+            spm = (aux - sph*3600)/60;
+            sps = (aux - sph * 3600 - spm * 60);
+
+
+
+             Console.WriteLine(pp);
+             Console.WriteLine("{0}:{1}:{2}", sph, spm, sps);
+        }
+    }
+}
